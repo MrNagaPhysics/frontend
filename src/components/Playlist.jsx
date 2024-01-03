b233b3116a74783926e5f09f2aeb136d09f535d3
Neil Martey <neil.marteybotchway@gmail.com>
2024-01-03 19:04:37 +0000

    Make playlist component work with new api

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -7,19 +7,18 @@ import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import BatteryChargingFullIcon from '@mui/icons-material/BatteryChargingFull';
 import { blue } from '@mui/material/colors';
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import PlaylistCategory from './PlaylistCatogory';
 
-const Playlist = ({ playlistName, playlistCategoriesData }) => {
-  // State to control w
+const Playlist = ({ playlistName, playlistCategories }) => {
+  // State to control whether a playlist section should collapse on click or expand. Defaults to being closed.
   const [open, setOpen] = useState(false);
   const handleClick = () => {
     setOpen(!open)
   }
-  console.log(playlistCategoriesData)
 
   return (
     <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
@@ -37,18 +36,18 @@ const Playlist = ({ playlistName, playlistCategoriesData }) => {
       </ListItemButton>
       <Collapse in={open} timeout="auto" unmountOnExit>
         {
-          playlistCategoriesData.map(
-            playlistCategoryData => {
-              const categoryTitle = playlistCategoryData['category_title']
-              const videoRange = playlistCategoryData['video_range']
-              const slug = playlistCategoryData['slug']
-              return <PlaylistCategory categoryTitle={categoryTitle} videoRange={videoRange} slug={slug}/>
+          playlistCategories.map(
+            playlistCategory => {
+              const playlistCategoryName = playlistCategory['category_title']
+              const id = playlistCategory['id']
+              const videoRange = playlistCategory['video_range']
+              const videos = playlistCategory['videos']
+              
+              return <PlaylistCategory playlistCategoryName={playlistCategoryName} key={id} videoRange={videoRange} videos={videos}/>
            }
 
           )
-        }
-  
-        
+        } 
       </Collapse>
       
     </List>
