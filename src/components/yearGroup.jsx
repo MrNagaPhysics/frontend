6daa8176844e79fe586af285cd8dd86b777ed41d
Neil Martey <neil.marteybotchway@gmail.com>
2024-01-03 18:57:00 +0000

    Make componenet yearGroup.jsx work with newer version of api

--- src/components/yearGroup.jsx
+++ src/components/yearGroup.jsx
@@ -2,19 +2,20 @@ import { Typography } from '@mui/material'
 import Paper from '@mui/material/Paper';
 import Playlist from './Playlist.jsx';
 
-const YearGroup = ({ yearGroupName, slug, playlistSet }) => {
-  // Component used to render a single Year Group
+const YearGroup = ({ yearGroupName, playlists }) => {
+  // Component used to render a single Year Group section on homepage
   return (
     <>
       <Paper elevation={2} pb={5}>
         <Typography variant='h5' py={2} px={2}textAlign='left'>{yearGroupName}</Typography>
         {
-          playlistSet.map(
+          playlists.map(
             playlist => {
-              const playlistName = playlistSet["playlist_name"]
-              const playlistCategoriesData = playlistSet["playlistcategory_set"]
-              console.log(playlist)
-              return <Playlist playlistName={playlistName} playlistCategoriesData = {playlistCategoriesData}/>
+              const playlistName = playlist["playlist_name"]
+              const playlistCategories = playlist["playlist_categories"]
+              const id = playlist['id']
+              
+              return <Playlist key={id} playlistName={playlistName} playlistCategories={playlistCategories}/>
               
             }
           )
