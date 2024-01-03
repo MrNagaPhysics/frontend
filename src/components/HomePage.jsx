2fe9760fe9e920079c2293e6774557c6f5caf77d
Neil Martey <neil.marteybotchway@gmail.com>
2024-01-03 18:50:32 +0000

    Make component HomePage.jsx work with live drf api

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 import YearGroup from './yearGroup'
 import { Typography } from '@mui/material'
 import Paper from '@mui/material/Paper';
-import data from './index.json';
 import { useEffect, useState} from "react";
 import QuizMenu from './QuizMenu';
+import axios from 'axios';
 
 const HomePage = () => {
-  //Get all year groups
-  const [indexData, setIndexData] = useState(null)
-  const [yearGroups, setYearGroups] = useState([])
-  const [mainResourceCategories, setMainResourceCategories] = useState([])
+  const baseUrl = "http://localhost:8000/main/apps/api/v1/"
+  const [yearGroups, setYearGroups] = useState(null) // Used to capture data associated with each year group (playlist, playlist categories, videos and worksheets)
+  const [mainResourceCategories, setMainResourceCategories] = useState(null) // Used to capture file data - main categories, sub categories and pdf files.
   
   useEffect(() => {
-    setIndexData(data)
-    setYearGroups(data["year_groups"])
-    setMainResourceCategories(data["main_resource_categories"])
+    axios
+      .get(baseUrl)
+      .then(
+        response => {
+          setYearGroups(response.data['year_groups'])
+          setMainResourceCategories(response.data['main_resource_categories'])
+        }
+      )
   }, [])
   
   // we need to iterate over each year group. then create a component called YearGroup. 
@@ -23,16 +27,16 @@ const HomePage = () => {
   // for a single year group, we will pass playlist set. we will need to iterate over each
   // playlist set, creating a new playlist componenet.
   // within the playlist componenet, we will have to pass playlist name and slug as props.
-  if (indexData !== null) {
+  if (yearGroups !== null && mainResourceCategories !== null) {
     return (
       <>
         {
           yearGroups.map(
             yearGroup => {
               const yearGroupName = yearGroup['year_group']
-              const slug = yearGroup['slug']
-              const playlistSet = yearGroup['playlist_set']
-              return <YearGroup yearGroupName={yearGroupName} slug={slug} playlistSet={playlistSet}/>
+              const id = yearGroup['id']
+              const playlists = yearGroup['playlists']
+              return <YearGroup key={id} yearGroupName={yearGroupName} playlists={playlists}/>
             }
           )
         }
