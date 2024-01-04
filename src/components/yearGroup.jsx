9462c3b1869c9f635d47b5349a3cdcd5ebad3a6c
Neil Martey <neil.marteybotchway@gmail.com>
2024-01-04 21:42:49 +0000

    fix merge

--- src/components/yearGroup.jsx
+++ src/components/yearGroup.jsx
@@ -4,34 +4,13 @@ import Playlist from './Playlist.jsx';
 import { useState } from 'react';
 import PlaylistData from './playlist.json';
 
-const YearGroup = (props) => {
-
-<<<<<<< HEAD
 const YearGroup = ({ yearGroupName, playlists }) => {
   // Component used to render a single Year Group section on homepage
-=======
-  // Component used to render a single Year Group
-
-  //States that deal with props
-  const [playlistSet,setPlaylistSet] = useState(props.playlistSet);
-  const [yearGroupName,setYearGroupName] = useState(props.yearGroupName);
-  // console.log(playlistSet);
-
-  //States that deal with imported JSON
-  const [playlist,setPlaylist] = useState(PlaylistData);
-  var PlCatData = [];
-  
-console.log(playlist)
-  //match playlistSet from props with playlist from JSON
-
-
->>>>>>> origin/main
   return (
     <>
       <Paper elevation={2} pb={5}>
         <Typography variant='h5' py={2} px={2}textAlign='left'>{yearGroupName}</Typography>
         {
-<<<<<<< HEAD
           playlists.map(
             playlist => {
               const playlistName = playlist["playlist_name"]
@@ -40,18 +19,6 @@ console.log(playlist)
               
               return <Playlist key={id} playlistName={playlistName} playlistCategories={playlistCategories}/>
               
-=======
-          playlistSet.map(
-            element => {
-              const playlistName = element.playlist_name;
-              var playlistCategoriesData = [];
-              if (playlistName === playlist.playlist_name){
-                playlistCategoriesData = playlist.playlistcategory_set
-              } else {
-                playlistCategoriesData = [];
-              }
-              return <Playlist playlistName={playlistName} playlistCategoriesData = {playlistCategoriesData}/>
->>>>>>> origin/main
             }
           )
         }
